934555cf2b920010178e49d09dbd1d6f02c0809c
koto <1>
2019-07-11 19:11:17 +0900

    aｄｄした後のコミット

---
+++ GameTemplate/Game/EnemyStatus.cpp
@@ -0,0 +1,21 @@
+#include "stdafx.h"
+#include "EnemyStatus.h"
+#include"EnemyChildren.h"
+
+
+EnemyStatus::EnemyStatus()
+{
+
+}
+EnemyStatus::~EnemyStatus()
+{
+
+}
+bool EnemyStatus::Start()
+{
+	return true;
+}
+void EnemyStatus::Update()
+{
+
+}
---
+++ GameTemplate/Game/EnemyStatus.h
@@ -0,0 +1,14 @@
+#pragma once
+class EnemyStatus:public IGameObject
+{
+public:
+	EnemyStatus();
+	~EnemyStatus();
+
+	bool Start();
+	void Update();
+
+
+
+};
+
